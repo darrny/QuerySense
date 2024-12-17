2adb4ba7fac4b4d9f6ca5bf2e151c08a42f099de
darrny <darren.lim.off@gmail.com>
2024-12-17 09:49:16 +0800

    extended the timing for the vercel function

--- src/app/api/analyze/route.ts
+++ src/app/api/analyze/route.ts
@@ -3,6 +3,7 @@ import { GoogleGenerativeAI } from '@google/generative-ai'
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
 
+export const maxDuration = 60;
 export async function POST(req: Request) {
     try {
         const { data, query } = await req.json()
